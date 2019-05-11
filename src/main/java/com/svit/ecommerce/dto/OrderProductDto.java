4d1bc36a9a65d328a416189e1de27d55ee833673
happydudu <happydudu1@gmail.com>
2019-05-11 11:19:36 -0700

    resources

---
+++ src/main/java/com/svit/ecommerce/dto/OrderProductDto.java
@@ -0,0 +1,25 @@
+package com.svit.ecommerce.dto;
+
+import com.svit.ecommerce.model.Product;
+
+public class OrderProductDto {
+
+    private Product product;
+    private Integer quantity;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into employee values(10001,'R&D001', 'Tom1');
+
+insert into employee values(10002,'R&D002', 'Tom2');
+
+insert into employee values(10003,'R&D003', 'Tom3');
+
+insert into employee values(10004,'R&D004', 'Tom4');
+
+insert into employee values(10005,'R&D005', 'Tom5');
